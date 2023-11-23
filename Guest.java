d17839c815f7ab8296fe2f3ac6febc7c269bff40
jinjja9 <150103366+jinjja9@users.noreply.github.com>
2023-11-23 15:44:43 +0700

    Add files via upload

---
+++ Guest.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Guest {
+    ArrayList<Object> DS = new ArrayList<>();
+    public void XemDS(ArrayList<Books> books);
+    for (int i = 0; i < Books; i++){
+
+        System.out.println("--------------------");
+    }
+    }
